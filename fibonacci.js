5b248e17f15a19e6d6c78a25657379c933c72b46
morelk19 <morelkhristian@gmail.com>
2022-09-21 13:42:57 -0400

    Updated Fib assignment

--- fibonacci.js
+++ fibonacci.js
@@ -3,26 +3,19 @@ const prompt = require('prompt-sync')({sigint: true});
 let n = Number(prompt(`Enter a value for "n": `));
 let count = 0;
 let sum1 = 0;
-let sum2 = 0;
+let sum2 = 1;
 let sum3 = 0;
 
 
 
 while(count !== n){
 
-    if(count === 0 ){
-        sum3 = sum1 + sum2;
-        sum2 = 1;
-        sum1 = sum3;
-    }else{
-        console.log(sum3);
-        sum3 = sum1 + sum2;
-        sum2 = sum1;
-        sum1 = sum3;
-    }
+    console.log(sum1);
+    sum3 = sum1 + sum2;
+    sum1 = sum2;
+    sum2 = sum3;
     count++;
 }
-console.log(sum3);
 
 
 
